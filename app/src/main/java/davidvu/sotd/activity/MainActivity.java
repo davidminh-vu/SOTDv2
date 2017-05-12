89e336ac89dbf56c1da19e86b7115345da85e09b
mvu <david.vu14@yahoo.de>
2017-05-12 15:36:44 +0200

    Navbar hinzugefügt

--- app/src/main/java/davidvu/sotd/activity/MainActivity.java
+++ app/src/main/java/davidvu/sotd/activity/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setTitle("Skill of the Day");
     }
-
+ //TOD
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
