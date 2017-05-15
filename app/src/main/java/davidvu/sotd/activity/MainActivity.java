5423643b20f409c688832620bbe038c690f5159d
mvu <david.vu14@yahoo.de>
2017-05-15 13:57:24 +0200

    Appfarbe verändert, weitere icons hinzugefügt/verändert

--- app/src/main/java/davidvu/sotd/activity/MainActivity.java
+++ app/src/main/java/davidvu/sotd/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package davidvu.sotd.activity;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -30,11 +32,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import davidvu.sotd.R;
+import davidvu.sotd.fragment.HomeFragment;
+import davidvu.sotd.fragment.SkillFragment;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
                 menuItem.setChecked(true);
                 mDrawerLayout.closeDrawers();
                 return true;
@@ -72,8 +81,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mtoolbar = (Toolbar) findViewById(R.id.mCustomToolbar);
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setTitle("Skill of the Day");
+        mtoolbar.setNavigationIcon(R.drawable.ic_dehaze_black_24dp);;
+
+        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_sort_black_24dp);
+        mtoolbar.setOverflowIcon(drawable);
     }
- //TOD
+ ////
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -90,4 +103,21 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     *  private Fragment getHomeFragment() {
+     switch (navItemIndex) {
+     case 0:
+     // home
+     HomeFragment homeFragment = new HomeFragment();
+     return homeFragment;
+     case 1:
+     // photos
+     SkillFragment skillFragment = new SkillFragment();
+     return skillFragment;
+     default:
+     return new HomeFragment();
+     }
+     }
+     */
 }
