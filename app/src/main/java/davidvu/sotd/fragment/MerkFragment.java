05ce795bff8f56159efc0f5f47c6f13a7261de2d
mvu <david.vu14@yahoo.de>
2017-05-20 17:31:39 +0200

    Fragments hinzugefügt

--- app/src/main/java/davidvu/sotd/fragment/MerkFragment.java
+++ app/src/main/java/davidvu/sotd/fragment/MerkFragment.java
@@ -13,12 +13,12 @@ import davidvu.sotd.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SkillFragment.OnFragmentInteractionListener} interface
+ * {@link MerkFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SkillFragment#newInstance} factory method to
+ * Use the {@link MerkFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SkillFragment extends Fragment {
+public class MerkFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class SkillFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SkillFragment() {
+    public MerkFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class SkillFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SkillFragment.
+     * @return A new instance of fragment MerkFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SkillFragment newInstance(String param1, String param2) {
-        SkillFragment fragment = new SkillFragment();
+    public static MerkFragment newInstance(String param1, String param2) {
+        MerkFragment fragment = new MerkFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class SkillFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_skill, container, false);
+        return inflater.inflate(R.layout.fragment_merk, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
