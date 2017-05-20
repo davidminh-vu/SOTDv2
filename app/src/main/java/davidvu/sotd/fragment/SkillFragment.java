32d4cfa823244186f14892c4aeaf4ce733e7b379
mvu <david.vu14@yahoo.de>
2017-05-20 17:34:53 +0200

    Fragments hinzugefügt

--- app/src/main/java/davidvu/sotd/fragment/SkillFragment.java
+++ app/src/main/java/davidvu/sotd/fragment/SkillFragment.java
@@ -19,6 +19,7 @@ import davidvu.sotd.R;
  * create an instance of this fragment.
  */
 public class SkillFragment extends Fragment {
+    // Vielleicht hast ja ein plan
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
