cfb4bf9b5a69869524946f098d0be23e18c985e2
mvu <david.vu14@yahoo.de>
2017-05-20 12:41:00 +0200

    Icons nachträglich hinzugefügt

---
+++ app/src/main/java/davidvu/sotd/fragment/HomeFragment.java
@@ -0,0 +1,9 @@
+package davidvu.sotd.fragment;
+
+/**
+ * Created by David on 15.05.2017.
+ *
+ */
+
+public class HomeFragment {
+}
---
+++ app/src/main/java/davidvu/sotd/fragment/SkillFragment.java
@@ -0,0 +1,8 @@
+package davidvu.sotd.fragment;
+
+/**
+ * Created by David on 15.05.2017.
+ */
+
+public class SkillFragment {
+}
